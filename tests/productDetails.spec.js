b1fde8cae6c2dff0d9b6974ec7db7a797a5785e9
Ruan-Portella <ruanmorales29@gmail.br>
2023-01-23 20:13:55 -0300

    Feat: Exercicio 08 Resolvido

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const productDetails = require('../src/productDetails');
+
 /*
   A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
 
@@ -29,15 +30,25 @@ const productDetails = require('../src/productDetails');
 
 */
 
+
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function')
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('Leite', 'The Boys'))).toBeTruthy()
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails.length).toBe(2)
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('Leite', 'The Boys')).toBe('object')
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(typeof productDetails[1]).not.toBe(productDetails[0])
     // Teste se os dois productIds terminam com 123.
+
+    const productDetail0 = productDetails('Leite', 'The Boys')[0].details.productId;
+    const productDetail1 = productDetails('Leite', 'The Boys')[1].details.productId;
+
+    expect(productDetail0 && productDetail1).toMatch('123');
   });
 });
